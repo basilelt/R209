50c544c0261d9a89d90fdd2c6fe017cc973d9398
basilelt <lethiecb@protonmail.com>
2023-03-27 15:22:23 +0200

    TD1

--- firstproject/myfirstapp/urls.py
+++ firstproject/myfirstapp/urls.py
@@ -3,4 +3,6 @@ from . import views
 
 urlpatterns = [
     path('index/', views.index),
+    path('formulaire/', views.formulaire),
+    path('bonjour/', views.bonjour),
 ]
--- firstproject/myfirstapp/views.py
+++ firstproject/myfirstapp/views.py
@@ -4,3 +4,12 @@ from django.shortcuts import render
 
 def index(request):
     return render(request, 'myfirstapp/index.html')
+
+def formulaire(request):
+    return render(request, 'myfirstapp/formulaire.html')
+
+def bonjour(request):
+    nom = request.GET["nom"] # récupère la valeur du paramètre nom du formulaire
+    prenom = request.GET["prenom"]
+    age = request.GET["age"]
+    return render(request, 'myfirstapp/bonjour.html', {"nom":nom, "prenom":prenom, "age":age}) # passe cette valeur à la vue au travers du dictionnaire de contexte
