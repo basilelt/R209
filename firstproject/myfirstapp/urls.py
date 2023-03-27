81f6a56062c462375f5d2d4bfa43d333f600713d
basilelt <lethiecb@protonmail.com>
2023-03-27 14:34:52 +0200

    TD1

--- firstproject/myfirstapp/urls.py
+++ firstproject/myfirstapp/urls.py
@@ -1,10 +1,6 @@
 from django.urls import path
 from . import views
-from django.contrib import admin
-from django.urls import path, include
 
 urlpatterns = [
-    path('index/', views.index),
-    path('admin/', admin.site.urls),
-    path('myfirstapp/', include('myfirstapp.urls'))
+    path('index/', views.index)
 ]
